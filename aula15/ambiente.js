bb800b63bb0bbde9577d4988699b9c33923b5aca
Gabriella-Aquino <gabyyaquino8@gmail.com>
2023-03-13 14:01:11 -0300

    vetores(aula15)

---
+++ aula15/ambiente.js
@@ -0,0 +1,36 @@
+/*
+let num = [1, 5, 9, 10]
+num[1]= 20
+num.push(25)
+num.push(400)
+console.log(`nosso vetor é ${num}`)
+*/
+
+let num = [1, 5, 3, 2, 4]
+/*
+num.sort()
+console.log(num)
+
+
+/*
+let fim = num.length -1
+console.log(num[fim])
+
+
+/*
+for(let x = 0; x<num.length; x++){
+    console.log(num[x])
+}
+
+
+/*
+for(let x in num){
+    console.log(num[x])
+}
+
+/*/
+let num2 = [1, 5, 3, 2, 4]
+let pos =num2.indexOf(3)
+console.log(pos)
+pos =num2.indexOf(7)
+console.log(pos)
